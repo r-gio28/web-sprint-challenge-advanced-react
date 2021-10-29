fdc5a7ecb6f7d74cbe490478c2495fecf89e2084
Giovanni  Rosales <giovannirosales@Giovannis-MacBook-Pro.local>
2021-10-29 04:55:55 -0500

    still working

--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -1,8 +1,7 @@
 // write your custom hook here to control your checkout form
-// import { useState } from 'react';
-import useLocalStorage from './../hooks/useLocalStorage';
+import { useState } from 'react';
 
-const useForm = (initialValues) =>{
+const useForm = () =>{
     //1. gather all stateful logic
     //2. return data need in component
     //3. connect our hook to our component
