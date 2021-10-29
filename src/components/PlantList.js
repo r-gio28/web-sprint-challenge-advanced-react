e0d109383deb26192ed861cb4915682edf19d7eb
Giovanni  Rosales <giovannirosales@Giovannis-MacBook-Pro.local>
2021-10-29 04:22:51 -0500

    plants are on the screen and able to be added into cart

--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -7,6 +7,18 @@ export default class PlantList extends Component {
     plants:[]
   }
   // when the component mounts:
+  componentDidMount() {
+    axios.get('http://localhost:3333/plants')
+    .then(resp => {
+      this.setState({
+        ...this.state.plants,
+        plants: resp.data
+      });
+    })
+    .catch(err => {
+      console.log(err);
+    })
+  }
   //   - fetch data from the server endpoint - http://localhost:3333/plants
   //   - set the returned plants array to this.state.plants
 
