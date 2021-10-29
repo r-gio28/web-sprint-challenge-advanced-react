841bf9701ffef3f734e76fa2f4429f6c929b9d49
Giovanni  Rosales <giovannirosales@Giovannis-MacBook-Pro.local>
2021-10-29 04:30:37 -0500

    cart checkout returns success message

--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -1,1 +1,26 @@
 // write your custom hook here to control your checkout form
+// import { useState } from 'react';
+import useLocalStorage from './../hooks/useLocalStorage';
+
+const useForm = (initialValues) =>{
+    //1. gather all stateful logic
+    //2. return data need in component
+    //3. connect our hook to our component
+    const [values, setValues] = useLocalStorage("form", initialValues);
+  
+    const handleChanges = e => {
+      setValues({
+        ...values,
+        [e.target.name]: e.target.value
+      });
+    };
+    
+    const clearForm = e => {
+      e.preventDefault();
+      setValues(initialValues);
+    };
+  
+    return([values, handleChanges, clearForm]);
+}
+
+export default useForm;
